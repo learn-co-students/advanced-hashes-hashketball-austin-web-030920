85443847e97f115c04e34db2b03a4c326e2e3f60
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-28 15:49:23 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,33 @@
-# Write your code here!
+require 'pry'
+require 'pp'
+
+def game_hash 
+  hashketball = {
+    home: {
+      team_name: "Brooklyn Nets", 
+      colors: ["Black", "White"], 
+      players: [
+        {player_name: "Alan Anderson", number: "0", shoe: "16", points: 22, rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks: 1},
+        {player_name: "", number: "", shoe: "", points: 0, rebounds: 0, assists: 0, steals: 0, blocks: 0, slam_dunks: 0},
+        {player_name: "", number: "", shoe: "", points: 0, rebounds: 0, assists: 0, steals: 0, blocks: 0, slam_dunks: 0},
+        {player_name: "", number: "", shoe: "", points: 0, rebounds: 0, assists: 0, steals: 0, blocks: 0, slam_dunks: 0},
+        {player_name: "", number: "", shoe: "", points: 0, rebounds: 0, assists: 0, steals: 0, blocks: 0, slam_dunks: 0}
+        ]}, 
+    
+    away: {
+      team_name: "", 
+      colors: [], 
+      players: [
+        {player_name: "", number: "", shoe: "", points: 0, rebounds: 0, assists: 0, steals: 0, blocks: 0, slam_dunks: 0},        
+        {player_name: "", number: "", shoe: "", points: 0, rebounds: 0, assists: 0, steals: 0, blocks: 0, slam_dunks: 0},
+        {player_name: "", number: "", shoe: "", points: 0, rebounds: 0, assists: 0, steals: 0, blocks: 0, slam_dunks: 0},
+        {player_name: "", number: "", shoe: "", points: 0, rebounds: 0, assists: 0, steals: 0, blocks: 0, slam_dunks: 0},
+        {player_name: "", number: "", shoe: "", points: 0, rebounds: 0, assists: 0, steals: 0, blocks: 0, slam_dunks: 0}]
+      
+  }}
 
 
-
-
+end
 
 
 
